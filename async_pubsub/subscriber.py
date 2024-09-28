c08eddda4c6e92a0059401a36798b2f2026deafb
Nanahuse <109294061+Nanahuse@users.noreply.github.com>
2024-09-28 17:12:41 +1200

    chore: dev tool update (#1)
    Package manager: uv
    Format & Lint: Ruff
    Type Check: Mypy

--- async_pubsub/subscriber.py
+++ async_pubsub/subscriber.py
@@ -1,29 +1,31 @@
 from __future__ import annotations
-from typing import Awaitable, Callable, Generic, TypeVar, TYPE_CHECKING
 
-from .key import Key
-from .topic import Topic
+from typing import TYPE_CHECKING, Generic, TypeVar
 
 if TYPE_CHECKING:
+    from collections.abc import Awaitable, Callable
+
     from .controller import Controller
+    from .key import Key
+    from .topic import Topic
 
 
 T = TypeVar("T")
 
 
 class Subscriber(Generic[T]):
-    def __init__(self, controller: Controller, topic: Topic[T], callback: Callable[[T], Awaitable[None]]):
+    def __init__(self, controller: Controller, topic: Topic[T], callback: Callable[[T], Awaitable[None]]) -> None:
         controller.add_subscriber(self)
-        self.__controller = controller
-        self.__topic = topic
-        self.__callback = callback
+        self._controller = controller
+        self._topic = topic
+        self._callback = callback
 
-    def __del__(self):
-        self.__controller.delete_subscriber(self)
+    def __del__(self) -> None:
+        self._controller.delete_subscriber(self)
 
-    def is_match(self, key: Key):
-        return self.__topic.key.match(key)
+    def is_match(self, key: Key) -> bool:
+        return self._topic.key.match(key)
 
-    async def subscribe(self, key: Key, value: T):
+    async def subscribe(self, key: Key, value: T) -> None:
         if self.is_match(key):
-            await self.__callback(value)
+            await self._callback(value)
